0748cd27dae89b210648c58c567de7a6376fe63c
2021102181 <2021102181@LAB401-008>
2023-05-05 19:07:27 -0300

    Adicionar arquivos de projeto.

---
+++ DocumentacaoOficial_UGB_PROG3_231/Data/DocContext.cs
@@ -0,0 +1,15 @@
+﻿using DocumentacaoOficial_UGB_PROG3_231.Models.BLOG;
+using DocumentacaoOficial_UGB_PROG3_231.Models.POST;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DocumentacaoOficial_UGB_PROG3_231.Data
+{
+    public class DocContext : DbContext
+    {
+        public DocContext(DbContextOptions<DocContext> options) : base(options) { }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
